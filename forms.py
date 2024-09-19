afbe520f387c2e1ef804ccb5343189a2efe67836
BOLAKAY <112257288+Mirfayoz01@users.noreply.github.com>
2024-09-19 15:10:57 +0500

    Delete forms.py

--- forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm
-from .models import Contact
-from django import forms
-
-
-class ContactForm(forms.ModelForm):
-    class Meta:
-        model = Contact
-        fields = ['name', 'email', 'message']
