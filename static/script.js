243020cd29d05544e869df0186f0a25e585ebeac
BOLAKAY <112257288+Mirfayoz01@users.noreply.github.com>
2024-09-20 17:01:11 +0500

    new

---
+++ static/script.js
@@ -0,0 +1,14 @@
+let currentSlide = 0;
+
+function moveSlide(direction) {
+    const slides = document.querySelector('.slides');
+    const totalSlides = slides.children.length;
+
+    currentSlide = (currentSlide + direction + totalSlides) % totalSlides;
+    slides.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+// Optional: Automatic slide
+setInterval(() => {
+    moveSlide(1);
+}, 5000);
